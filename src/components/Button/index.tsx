80542f3804e2951b86fb27b2b62f0f4b05554149
nataliaaraujo0 <devjrteste@gmail.com>
2022-03-02 19:12:51 -0300

    chore: valores default compnent button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+interface Props {
+  backgroundColor?: string;
+  color: string;
+}
 
 const style: React.CSSProperties = {
   background: "#7159c1",
   color: "#fff",
 };
-export const Button: React.FC = ({ children }) => {
+export const Button: React.FC<Props> = ({
+  children,
+  backgroundColor = "#47328a",
+  color,
+}) => {
   return <button style={style}>{children}</button>;
 };
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -2,4 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { Button } from "../src";
 
-storiesOf("Button", module).add("Default", () => <Button />);
+storiesOf("Button", module).add("Default", () => (
+  <Button backgroundColor="#7159c1" color="#fff" />
+));
