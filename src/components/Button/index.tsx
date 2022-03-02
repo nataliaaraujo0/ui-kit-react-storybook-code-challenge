7c64cab59ad3af3cce65753de03a11bfe38bfcd4
nataliaaraujo0 <devjrteste@gmail.com>
2022-03-02 18:18:42 -0300

    first commit

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const style: React.CSSProperties = {
+  background: "#7159c1",
+  color: "#fff",
+};
+export const Button: React.FC = ({ children }) => {
+  return <button style={style}>{children}</button>;
+};
