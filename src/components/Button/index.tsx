9a9e1c4c6a595930231843c661dc4aa94ece1a86
nataliaaraujo0 <devjrteste@gmail.com>
2022-03-03 14:16:43 -0300

    chore: separando props css do button para um arquivo styled

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,15 @@
 import React from "react";
+import { Container } from "./styled";
 interface Props {
   /** This is my favorite prop background */
   backgroundColor?: string;
   color: string;
 }
 
-const style: React.CSSProperties = {
-  background: "#7159c1",
-  color: "#fff",
-};
 export const Button: React.FC<Props> = ({
   children,
   backgroundColor = "#47328a",
   color,
 }) => {
-  return <button style={style}>{children}</button>;
+  return <Container>{children}</Container>;
 };
---
+++ src/components/Button/styled.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.hr`
+  background-color: #7159c1;
+  color: #fff;
+`;
