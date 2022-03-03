a6a78d3612db3020837f845a8081c3cf33d3359b
nataliaaraujo0 <devjrteste@gmail.com>
2022-03-03 14:49:16 -0300

    chore: criando outros tipos de button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,32 @@
 import React from "react";
+
 import { Container } from "./styled";
+
 export interface Props {
-  /** This is my favorite prop background */
   backgroundColor?: string;
-  color: string;
+  color?: string;
+  outlined?: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
-export const Button: React.FC<Props> = ({
+const ButtonWrapper: React.FC<Props> = ({
   children,
-  backgroundColor = "#47328a",
-  color,
+  backgroundColor = "#7159c1",
+  color = "#fff",
+  outlined = false,
+  ...props
 }) => {
   return (
-    <Container backgroundColor={backgroundColor} color={color}>
+    <Container
+      backgroundColor={backgroundColor}
+      color={color}
+      outlined={outlined}
+      {...props}
+    >
       {children}
     </Container>
   );
 };
+
+export { ButtonWrapper as Button };
--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
+
 import { Button } from "../src";
 
-storiesOf("Button", module).add("Default", () => (
-  <Button backgroundColor="#7159c1" color="#fff" />
-));
+storiesOf("Button", module)
+  .add("Default", () => <Button>Default</Button>)
+  .add("Outlined", () => <Button outlined>Outlined</Button>)
+  .add("Disabled", () => <Button disabled>Default</Button>);
